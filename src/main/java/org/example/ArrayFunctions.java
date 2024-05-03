64868def599d75de7933e26b74d8874f7e0d411a
luca-trnka <lucie-trnkova76@seznam.cz>
2024-05-03 09:44:05 +0200

    second function done: creation of array set by user inputs

--- src/main/java/org/example/ArrayFunctions.java
+++ src/main/java/org/example/ArrayFunctions.java
@@ -1,18 +1,32 @@
 package org.example;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class ArrayFunctions {
 
-    //Creation of random array with random length (1-10) and random ints with values (1-100)
+    //Creation of random array with random length (1-10) and random ints with values (-100 -> +100)
     public static int[] creationOfRandomArray() {
         Random random = new Random();
         int max_length_size = 10;
-        int max_int_size = 100;
+        int max_int_size = 200;
         int length = random.nextInt(max_length_size) + 1;
         int[] array = new int[length];
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(max_int_size) + 1;
+            array[i] = random.nextInt(max_int_size) - 100;
+        }
+        return array;
+    }
+
+    //Creation of array with length and numbers set by user inputs
+    public static int[] creationOfArraySetByUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of array: ");
+        int length = scanner.nextInt();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            System.out.print("Enter the number on position " + (i) + ": ");
+            array[i] = scanner.nextInt();
         }
         return array;
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = ArrayFunctions.creationOfRandomArray();
-        System.out.println(Arrays.toString(array));
+        int[] array1 = ArrayFunctions.creationOfRandomArray();
+        System.out.println(Arrays.toString(array1));
+
+        int[] array2 = ArrayFunctions.creationOfArraySetByUser();
+        System.out.println(Arrays.toString(array2));
     }
 }
